898d6435ecd1aa7ab750e090954ba58fd134e4c4
diakivvasyl <diakivvasyl@gmail.com>
2018-11-26 02:51:04 +0200

    Light update.

--- HM5/task2.php
+++ HM5/task2.php
@@ -2,6 +2,7 @@
 <meta charset='utf-8'>
 <html>
 <head>
+    <title>Task 2</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
@@ -20,15 +21,15 @@
 echo '<br>';
 echo '<div class="container">';
 echo '<br>';
-echo '<h3> Введіть число </h3>';
-echo '<form action="index.php" method="post">
+echo '<h3> Enter a number </h3>';
+echo '<form action="/?file=task2.php" method="post">
 <label for="catFirst">catFirst</label>
      <input type="number" name="catFirst">
      <label for="catSec">catSec</label>
      <input type="number" name="catSec">
      <label for="hypo">hypo</label>
      <input type="number" name="hypo"> 
-     <input type="submit">
+     <input type="submit" value="Send">
      </form>';
 
 function pythagoras($catFirst=2, $catSec=4, $hypo=false, $precision=2)
--- HM5/task3.php
+++ HM5/task3.php
@@ -2,6 +2,7 @@
 <meta charset='utf-8'>
 <html>
 <head>
+    <title>Task 3</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
@@ -20,10 +21,10 @@
 echo '<br>';
 echo '<div class="container">';
 echo '<br>';
-echo '<h3> Введіть число </h3>';
-echo '<form action="index.php" method="post">
+echo '<h3> Enter a number </h3>';
+echo '<form action="/?file=task3.php" method="post">
      <input type="number" name="number">
-     <input type="submit">
+     <input type="submit" value="send">
      </form>';
 
 function sumNumber()
